7006882cc02f4475a42e9995346adb865697601d
gene9831 <pacify.98@gmail.com>
2026-08-09 21:11:13 +0800

    fix(extension-manager): publish composable barrel exports

--- packages/components/src/extension-manager/composables/index.ts
+++ packages/components/src/extension-manager/composables/index.ts
@@ -1,2 +1,4 @@
 export { useAsChild } from './useAsChild'
 export { useExtensionCardPopoverPosition } from './useExtensionCardPopoverPosition'
+export { provideExtensionListContext, useExtensionListContext } from './useExtensionListContext'
+export { provideExtensionManagerContext, useExtensionManager, useExtensionManagerContext } from './useExtensionManager'
