95b19f21c0e358c942ea56227e34af1287a10c6e
gene9831 <pacify.98@gmail.com>
2026-08-13 19:00:41 +0800

    fix(extension-manager): remove stale list context export

--- packages/components/src/extension-manager/composables/index.ts
+++ packages/components/src/extension-manager/composables/index.ts
@@ -1,4 +1,3 @@
 export { useAsChild } from './useAsChild'
 export { useExtensionCardPopoverPosition } from './useExtensionCardPopoverPosition'
-export { provideExtensionListContext, useExtensionListContext } from './useExtensionListContext'
 export { useExtensionContext } from './useExtensionContext'
